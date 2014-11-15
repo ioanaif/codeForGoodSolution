0337303f39ed6318e2eb1f243f5374fc922e45ce
radubl <radu.blana@yahoo.com>
2014-11-15 06:19:18 +0000

    refactored thingy

--- backend/index.php
+++ backend/index.php
@@ -1,16 +1,35 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <meta charset='utf-8'>
-    <meta name="description" content="Codeforgoodsolution : Nobel prize thingy">
-
-    <link rel="stylesheet" type="text/css" media="screen" href="stylesheets/index.css">
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <script type="text/javascript" src="javascripts/main.js"></script>
-    <title>NobelGraph</title>
+	<meta charset='utf-8'>
+	<meta name="description" content="Codeforgoodsolution : Nobel prize thingy">
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,400,300,600,700' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" type="text/css" media="screen" href="stylesheets/index.css">
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	<script type="text/javascript" src="javascripts/main.js"></script>
+	<title>NobelGraph</title>
   </head>
 
   <body>
-    Hello World.
+  	<div id="mainContainer">
+		<div id="quizCanvas">
+			<div class="container">
+				<input id="mainInput" class="transition" type="text" placeholder="Which Nobel Prize Laureate are You?">
+				<span id="triangle"></span>
+			</div>
+			<div class="rightContainer">
+				<div id="helpYou" class="transition-short">
+					Let Us help You.
+				</div>
+				<div class="questionCanvas">
+					<div class="questionTitle">Feel Sciency?</div>
+					<div class="answerButton transition-short">Sure</div>
+					<div class="answerButton transition-short">Not Really</div>
+				</div>
+			</div>
+			
+		</div>  		
+  	</div>
+
   </body>
 </html>
--- backend/javascripts/main.js
+++ backend/javascripts/main.js
@@ -1,7 +1,35 @@
-console.log('This would be the main JS file.');
+// console.log('This would be the main JS file.');
 
 $(document).ready(function () {
-	getAllLaureates ();
+
+	$('#mainInput').css({'opacity': 1, 'margin-top':'100px'});
+
+	setTimeout(function () {
+			$('#helpYou').css({'opacity': 1});
+	},600);
+
+	$(document).on('click','#helpYou', function () {
+
+		$('#helpYou').css({'opacity': 0, 'margin-top':'-50px'});
+		
+		setTimeout(function () {
+			$('.questionCanvas').fadeIn();
+		},300);
+		
+	})
+
+	$(document).on('focus','#mainInput', function () {
+
+		$('#mainInput').attr('placeholder', '');
+
+	});
+
+	$(document).on('blur','#mainInput', function () {
+
+		$('#mainInput').attr('placeholder', 'Which Nobel Prize Laureate are You?');
+
+	});
+
 });
 
 function getAllLaureates () {
@@ -13,7 +41,57 @@ function getAllLaureates () {
 				$(document).ready(function() 
 				{
 					var laureates = JSON.parse(data);
+					//console.log(laureates);
+					return laureates;
+				});
+			});
+
+}
+
+function getAllLaureatesByCountry (country) {
+		$.post('fetchLaureates.php', {
+			'token' : 'country',
+			'country' : country
+			}, function(data) 
+			{
+				//alert(data)
+				$(document).ready(function() 
+				{
+					var laureates = JSON.parse(data);
 					console.log(laureates);
+					//return laureates;
 				});
 			});
 }
+
+
+// // level1
+// var level1Questions = ["Are you a people-oriented person?","Do you consider yourself to be an artsy type?","Do you see the world in black and white or shades of grey?"];
+// var level1AnswerA = ["Definetely","Maybe","Shades"];
+// var level1AnswerB = ["People? What are those?","Not really","Black and White"];
+// // level2 question[0] - [2] are for Humanities  level two, [3] - [5] are for Ph-CH(a) / EcMed(b)
+// var level2Questions = ["Do ethical issues concern you?","Would you rather help others by donating or by volunteering?","Do you enjoy reading?", "Are you driven more by financial gain or curiosity?", ""];
+// var level2AnswerA = ["Yes","Volunteering","Not really", "Curiosity", ];
+// var level2AnswerB = ["Not really","Donating","Yes, I do.", "Financial"]; 
+// //level 3 
+// var level3Questions = [];
+// var level3AnswerA = [];
+// var level3AnswerB = [];
+// // level 4
+// var level4Questions = ["What's your favourite car?", "Which was the most revolutionary technology?",  ];
+// var level4AnwserA = ["Ford T", "Radio", ];
+// var level4AnwserB = ["Tesla S", "TV, "];
+// // level 5
+// var level5Questions = ["What is your favourite book ?"];
+// var level5AnswerA = ["Lord of the rings"];
+// var level5Answer = ["Harry Potter"];
+// // level 6 4 paints, question depends on year from level 5 question
+// var level6Questions = [];
+// var level6AnswerA = [];
+// var level6AnswerB = [];
+
+// function nextQuestion(){
+// 	for(int i = 0;i<=7;i++)
+// 		if (i )
+	
+// 	}
