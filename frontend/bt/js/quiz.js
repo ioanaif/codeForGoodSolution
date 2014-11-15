aa9ff367a6b2c3d838f4be30d7008204742f475b
dalexro <alexander.danila@gmail.com>
2014-11-15 02:30:04 +0000

    commit 6 - script js

---
+++ frontend/bt/js/quiz.js
@@ -0,0 +1,25 @@
+
+
+function renderQuestion() {
+  
+    document.writeln('<p class="question">' + Test + ' <span id="result_' + a + '"><img src="blank.gif" style="border:0" alt="" /></span></p>');
+
+    }
+  
+
+
+/*
+  function generateQuestions(){
+
+  	html = '<div>' + 
+
+  	document.getElementByID('quiz-container').innerHTML = html;
+
+
+  }
+
+  $(document).on('click', '#questionButton', function(){
+
+  });
+
+  */
---
+++ frontend/quiz.js
@@ -0,0 +1,24 @@
+
+
+function renderQuestion() {
+  
+    document.writeln('<p class="question">' + Test + '</p>'); 
+    }
+  
+
+
+/*
+  function generateQuestions(){
+
+  	html = '<div>' + 
+
+  	document.getElementByID('quiz-container').innerHTML = html;
+
+
+  }
+
+  $(document).on('click', '#questionButton', function(){
+
+  });
+
+  */
